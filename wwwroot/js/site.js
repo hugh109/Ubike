d3341a95a185a6288058be0e3bca90512e197ab4
hugh109 <hugh117@gmail.com>
2018-07-12 16:48:24 +0800

    測試: 去除 node_modules

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Write your JavaScript code.
+var value1, value2, value3, value4;
+value1 = 1;
+value2 = 2;
+value3 = 3;
+value4 = 4;
+const x = (value1 && value2) || (value3 && value4);
+function testShow() {
+  alert(1111);
+  alter(222);
+}
